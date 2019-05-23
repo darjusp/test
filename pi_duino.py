33916edc1576c11bff346a2854bd78a30e9117e3
Darjus <darius_p@live.com>
2019-05-23 21:08:18 +0300

    Update pi_duino.py

--- pi_duino.py
+++ pi_duino.py
@@ -1,6 +1,15 @@
 import serial
+from mpd import MPDClient
+
+client = MPDClient()
+client.add("a2002011001-e02.wav")
+client.play()
+
+
 s1 = serial.Serial('/dev/ttyACM0',9600,timeout=5)
 s1.flushInput()
+
+
 
 while True:
         if s1.inWaiting()>0:
